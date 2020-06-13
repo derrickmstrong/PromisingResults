738e7d28d86b0d10f78c9c776d81b030ec66ff35
Derrick Strong <derrickmstrong@gmail.com>
2020-06-13 16:58:17 -0500

    Completed lab

--- app.js
+++ app.js
@@ -88,11 +88,30 @@ slowMath
   });
 
 // Async/Await
+const doMath = async () => {
+  try {
+    let result = await slowMath.add(6, 2);
+    console.log(result);
+    result = await slowMath.multiply(result, 2);
+    console.log(result);
+    result = await slowMath.divide(result, 4);
+    console.log(result);
+    result = await slowMath.subtract(result, 3);
+    console.log(result);
+    result = await slowMath.add(result, 98);
+    console.log(result);
+    result = await slowMath.remainder(result, 2);
+    console.log(result);
+    result = await slowMath.multiply(result, 50);
+    console.log(result);
+    result = await slowMath.remainder(result, 40);
+    console.log(result);
+    result = await slowMath.add(result, 32);
+    console.log(result);
+    console.log(`The final result is ${result}.`);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const doMath = async() => {
-    try {
-        let sum = await ''
-    } catch (error) {
-        
-    }
-}
+doMath();
